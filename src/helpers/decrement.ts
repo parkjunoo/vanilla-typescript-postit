bdd19e4f06432f3d2657373463380ed66cfeb4c2
parkjunsoo <01junsoo@gmail.com>
2022-04-08 00:20:32 +0900

    feat: init server

---
+++ src/helpers/decrement.ts
@@ -0,0 +1,6 @@
+// eslint-disable-next-line no-plusplus
+const decrement = (value: number) => --value
+
+export default decrement
+
+
---
+++ src/helpers/increment.ts
@@ -0,0 +1,4 @@
+// eslint-disable-next-line no-plusplus
+const increment = (value: number) => ++value;
+
+export default increment;
