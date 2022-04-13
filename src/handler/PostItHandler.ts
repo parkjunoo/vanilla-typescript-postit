a97a61004469aea4470acedb2059e7d81c2c9b1a
parkjunsoo <01junsoo@gmail.com>
2022-04-13 14:35:56 +0900

    feat: 디자인 변경

--- src/handler/PostItHandler.ts
+++ src/handler/PostItHandler.ts
@@ -1,5 +1,22 @@
+export default class PostItHandler {
+  $postit_stk: HTMLDivElement;
+  constructor($target: HTMLDivElement) {
+    this.$postit_stk = $target;
+    this.drawPostits();
+  }
 
-
-class PostItHandler {
-  constructor() {}
+  drawPostits(): void {
+    console.log("!!!!!!!!!");
+    // const postitList = document.querySelectorAll(".postit");
+    // postitList.forEach(function (postit: HTMLDivElement, idx: number) {
+    //   let priority: string = postit.getAttribute("priority");
+    //   if (!priority) {
+    //     priority = idx + 1;
+    //     postit.setAttribute("priority", priority);
+    //   }
+    //   postit.style.zIndex = priority;
+    //   return;
+    //   // postit.addEventListener("mousedown", handleMouseDown);
+    // });
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,51 @@
-import {
-  handleMouseDown,
-  handleMouseMove,
-  handleMouseUp,
-} from "./handler/movePostitHandler";
+import PostItHandler from "./handler/PostItHandler";
 
-const postitStk = document.querySelector(".postit-stk");
-const app = document.querySelector(".main-section");
+const $postitStk: HTMLDivElement = document.querySelector(".postit-stk")!;
+const postItHandler = new PostItHandler($postitStk);
+// const app = document.querySelector(".main-section");
 
-postitStk?.addEventListener("drop", onDragDrop);
-postitStk?.addEventListener("dragend", onDragEnd);
+// postitStk?.addEventListener("drop", onDragDrop);
+// postitStk?.addEventListener("dragend", onDragEnd);
 
-app?.addEventListener("dragover", onDrag);
-function drowPostit() {
-  const postitList = document.querySelectorAll(".postit");
-  postitList.forEach(function (postit: any, idx) {
-    let priority: any = postit.getAttribute("priority");
-    if (!priority) {
-      priority = idx + 1;
-      postit.setAttribute("priority", priority);
-    }
-    postit.style["z-index"] = priority;
+// app?.addEventListener("dragover", onDrag);
+// function drowPostit() {
+//   const postitList = document.querySelectorAll(".postit");
+//   postitList.forEach(function (postit: any, idx) {
+//     let priority: any = postit.getAttribute("priority");
+//     if (!priority) {
+//       priority = idx + 1;
+//       postit.setAttribute("priority", priority);
+//     }
+//     postit.style["z-index"] = priority;
 
-    postit.addEventListener("mousedown", handleMouseDown);
-  });
-}
+//     postit.addEventListener("mousedown", handleMouseDown);
+//   });
+// }
 
-document.addEventListener("mouseup", handleMouseUp);
+// document.addEventListener("mouseup", handleMouseUp);
 
-function onDragDrop(event: any) {}
+// function onDragDrop(event: any) {}
 
-function onDragEnd(e: any) {
-  console.log("!!!!!!!!!!!!!!!!!!", e);
-  const newPostit = document.createElement("div");
-  newPostit.classList.add("postit");
-  newPostit.innerHTML = `
-  <div class="postit-top-area" ></div>
-  <div
-  class="postit-contents-area"
-  >안녕하세요</div>
-  `;
-  app?.appendChild(newPostit);
-  const mouseX = e.clientX;
-  const mouseY = e.clientY;
+// function onDragEnd(e: any) {
+//   console.log("!!!!!!!!!!!!!!!!!!", e);
+//   const newPostit = document.createElement("div");
+//   newPostit.classList.add("postit");
+//   newPostit.innerHTML = `
+//   <div class="postit-top-area" ></div>
+//   <div
+//   class="postit-contents-area"
+//   >안녕하세요</div>
+//   `;
+//   app?.appendChild(newPostit);
+//   const mouseX = e.clientX;
+//   const mouseY = e.clientY;
 
-  newPostit.style.left = mouseX + "px";
-  newPostit.style.top = mouseY + "px";
-  drowPostit();
-}
+//   newPostit.style.left = mouseX + "px";
+//   newPostit.style.top = mouseY + "px";
+//   drowPostit();
+// }
 
-function onDrag(e: any) {
-  console.log(e.target);
-}
-drowPostit();
+// function onDrag(e: any) {
+//   console.log(e.target);
+// }
+// drowPostit();
